6e26bc390c60510ab5dcfffd6bb0ca42e9bac361
Trungpdt12 <112165384+Trungpdt12@users.noreply.github.com>
2024-04-13 00:16:26 +0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Shop management
+//
+//  Created by MacBook Air on 05/04/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
